249b5476717f7b62ab26df9b6f0e752814afe029
calv4n <calvin.pfrender@gmail.com>
2025-05-09 23:07:43 +0200

    footer beginning

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,8 +1,29 @@
 export default function Footer() {
 
     return (
-        <div>
-            
-        </div>
+        <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+            <aside>
+                <Image>
+
+                </Image>
+                <p>
+                    Holzabbund Wädenswil AG
+                    <br />
+                    Abbundwerk seit 1987
+                </p>
+            </aside>
+            <nav>
+                <h6>Holzabbund Angebot</h6>
+                <a href="">Produkte</a>
+                <a href="">Planung</a>
+                <a href="">Dienstleistungen</a>
+                <a href="">Lieferanten</a>
+                <a href="">Standort</a>
+            </nav>
+            <nav>
+                <h6>Anschrift & Kontakt</h6>
+                
+            </nav>
+        </footer>
     )
 }
