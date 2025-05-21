03af806e3d5e750ea21a3988733c26f3aaca3815
calv4n <calvin.pfrender@gmail.com>
2025-05-21 16:13:01 +0200

    daisy ui import fix and footer almost done

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,11 +1,17 @@
+import Image from "next/image"
+
 export default function Footer() {
 
     return (
-        <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-            <aside>
-                <Image>
-
-                </Image>
+        <footer className="footer sm:footer-horizontal bg-white/95 text-gray-600 p-10 ">
+            <aside className="w-50">
+                <Image
+                    src="/img/logo.png"
+                    alt="Holzabbund"
+                    width={100}
+                    height={100}
+                    className="object-contain"
+                />
                 <p>
                     Holzabbund Wädenswil AG
                     <br />
@@ -13,16 +19,29 @@ export default function Footer() {
                 </p>
             </aside>
             <nav>
-                <h6>Holzabbund Angebot</h6>
-                <a href="">Produkte</a>
-                <a href="">Planung</a>
-                <a href="">Dienstleistungen</a>
-                <a href="">Lieferanten</a>
-                <a href="">Standort</a>
+                <h6 className="font-bold text-gray-800">Holzabbund Wädenswil AG</h6>
+                <a className="link link-hover" href="">Startseite</a>
+                <a className="link link-hover" href="">Über uns</a>
+                <a className="link link-hover" href="">Offene Stellen</a>
+                <a className="link link-hover" href="">Datenschutz</a>
+                <a className="link link-hover" href="">Impressum</a>
+                <a className="link link-hover" href="">Kontakt</a>
             </nav>
             <nav>
-                <h6>Anschrift & Kontakt</h6>
-                
+                <h6 className="font-bold text-gray-800">Holzabbund Angebot</h6>
+                <a className="link link-hover" href="">Produkte</a>
+                <a className="link link-hover" href="">Planung</a>
+                <a className="link link-hover" href="">Dienstleistungen</a>
+                <a className="link link-hover" href="">Lieferanten</a>
+                <a className="link link-hover" href="">Standort</a>
+            </nav>
+            <nav>
+                <h6 className="font-bold text-gray-800">Anschrift & Kontakt</h6>
+                <a className="link link-hover" href="">Holzabbund Wädenswil AG</a>
+                <a className="link link-hover" href="">Aamühle 9</a>
+                <a className="link link-hover" href="">8820 Wädenswil</a>
+                <a className="link link-hover" href="">Tel. 044 683 21 65</a>
+                <a className="link link-hover" href="">Fax 044 683 21 69</a>
             </nav>
         </footer>
     )
