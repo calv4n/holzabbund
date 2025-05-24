66759e35b66b64483fa3fb2dd83d500271e6dbe6
calv4n <calvin.pfrender@gmail.com>
2025-05-24 15:10:32 +0200

    fixed images. bootstrap css bug still open

--- src/app/components/BootstrapCarousel.tsx
+++ src/app/components/BootstrapCarousel.tsx
@@ -1,32 +1,29 @@
 "use client";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import Carousel from "react-bootstrap/Carousel";
 import Image from "next/image";
 
 const images = [
-  { src: "/img/slide1.jpg", label: "First slide", text: "Nulla vitae elit libero, a pharetra augue mollis interdum." },
-  { src: "/img/slide2.png", label: "Second slide", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { src: "/img/slide3.png", label: "Third slide", text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur." }
+  { src: "/img/slide1.jpg" },
+  { src: "/img/slide2.jpg" },
+  { src: "/img/slide3.jpg" }
 ];
 
 export default function BootstrapCarousel() {
   return (
-    <div className="w-full max-w-3xl mx-auto mt-10 border-[6px] border-dashed border-black rounded-3xl overflow-hidden shadow-xl">
+    <div className="w-full max-w-3xl mx-auto mt-10 border-black overflow-hidden shadow-2xl">
       <Carousel fade>
         {images.map((img, idx) => (
           <Carousel.Item key={idx} interval={5000}>
-            <div className="relative w-full h-[500px]">
+            <div className="relative w-full h-[500px] w-[800px]">
               <Image
                 src={img.src}
-                alt={img.label}
+                alt="Holzabbund Wädenswil"
                 fill
                 className="object-cover"
               />
             </div>
-            <Carousel.Caption>
-              <h3>{img.label}</h3>
-              <p>{img.text}</p>
-            </Carousel.Caption>
           </Carousel.Item>
         ))}
       </Carousel>
