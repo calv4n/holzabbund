d8483cf24136ba7eae5434487bf658eb36cc76fc
calv4n <calvin.pfrender@gmail.com>
2025-05-26 19:27:08 +0200

    spotlight cards almost done.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,21 @@
 import Link from "next/link";
 import CustomCarousel from "@/app/components/customcarousel";
 import ShinyText from "@/components/ui/shinytext";
+import SpotlightCard from "@/components/ui/spotlightcard";
 
 const overTitle = "Abbundwerk in Wädenswil";
 const title = "Abbundarbeiten nach Mass";
 const titleDescription = "Von der Balkenlagen über Dachstöcke, Binder und Elementbauten. Mit Leidenschaft zu unserem Beruf und zum Holz. Kompetent und fachmännisch.";
+const spotlightCards = [
+  {
+    title: "Abbund",
+    text: "Carports, Anbauten oder Elementbauten. Wir kriegen es hin. Aus Leidenschaft als Zimmermann mit unserer modernen Abbundanlage gefertigt.",
+  },
+  {
+    title: "Holzverkauf",
+    text: "Neben Abbund- und Planungsarbeiten bieten wir auch Holzprodukte in sägeroher und gehobelter Ware zum Verkauf an.",
+  },
+];
 
 export default function Startseite() {
   return (
@@ -31,6 +42,16 @@ export default function Startseite() {
           />
         </Link>{" "}
         auf!
+      </div>
+      <div>
+        <div className="flex flex-row mt-15 justify-center items-center gap-8">
+          {spotlightCards.map((card, index) => (
+            <SpotlightCard key={index} className="h-[220px] max-w-[800px] mx-auto p-10">
+              <h2 className="text-3xl font-bold text-[#b43300] mb-5">{card.title}</h2>
+              <p className="text-lg text-gray-600">{card.text}</p>
+            </SpotlightCard>
+          ))}
+        </div>
       </div>
     </div>
   );
